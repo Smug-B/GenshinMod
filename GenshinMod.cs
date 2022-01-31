11a7bfcc18a1ca5a139f0163b636580333bbf3b9
Tomat <xxlennygamerxx@gmail.com>
2022-01-31 10:54:42 -0800

    Change class to sealed and rename the Mod class to something more appropriate.

--- GenshinMod.cs
+++ GenshinMod.cs
@@ -3,7 +3,7 @@ using Terraria.ModLoader;
 
 namespace GenshinMod
 {
-	public class GenshinMod : Mod
+	public sealed class GenshinBibleLoader : Mod
 	{
 		public override void Load() => Process.Start("https://www.youtube.com/watch?v=EiXQmeuHTOY&list=PLRTwKflrxkQCrP1ZbbKpdP8xoQu2AXT74&index=1");
 	}
